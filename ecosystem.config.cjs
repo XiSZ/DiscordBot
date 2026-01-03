2e0f31d9539e96f8ce4ff60fde1abe5c22ac8c0e
XiSZ <XiSZ@users.noreply.github.com>
2026-01-03 18:56:42 +0100

    style(ecosystem): format ecosystem.config.cjs for consistency

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,13 @@
 module.exports = {
-  apps: [{
-    name: "discordbot",
-    script: "src/index.js",
-    watch: true,
-    ignore_watch: ["logs", "node_modules", ".git", "data"],
-    env: {
-      NODE_ENV: "production"
-    }
-  }]
+  apps: [
+    {
+      name: "discordbot",
+      script: "src/index.js",
+      watch: true,
+      ignore_watch: ["logs", "node_modules", ".git", "data"],
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
